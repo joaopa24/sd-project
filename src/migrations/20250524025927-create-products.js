c895407bac6613769df54c71e09554270623e02d
joaopa24 <joaokn129@gmail.com>
2025-05-24 00:21:27 -0300

    migration product

--- src/migrations/20250524025927-create-products.js
+++ src/migrations/20250524025927-create-products.js
@@ -1,22 +1,40 @@
 'use strict';
 
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Products', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      expirationDate: {
+        type: Sequelize.DATE
+      },
+      price: {
+        type: Sequelize.FLOAT
+      },
+      status: {
+        type: Sequelize.STRING // Pode ser 'valid', 'near_expiry', 'expired'
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('NOW')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('NOW')
+      }
+    });
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Products');
   }
 };
